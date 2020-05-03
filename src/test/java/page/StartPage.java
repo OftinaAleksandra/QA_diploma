6e94f91415b07fbc5ab1ede00b24dfc42b0c4155
Aleksandra Oftina <aleksandra.oftin@mail.ru>
2020-05-03 22:35:16 +0300

    added tests

--- src/test/java/page/StartPage.java
+++ src/test/java/page/StartPage.java
@@ -6,23 +6,32 @@ import com.codeborne.selenide.SelenideElement;
 import data.Cards;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class StartPage {
     SelenideElement buyButton = $$("button").find(exactText("Купить"));
     SelenideElement creditButton = $$("button").find(exactText("Купить в кредит"));
-    ElementsCollection form = $$(".input");
-    public SelenideElement numberCardField = form.find(exactText("Номер карты")).$(".input__control");
-    public SelenideElement monthField = form.find(exactText("Месяц")).$(".input__control");
-    public SelenideElement yearField = form.find(exactText("Год")).$(".input__control");
-    public SelenideElement ownerField = form.find(exactText("Владелец")).$(".input__control");
-    public SelenideElement cvcCvvField = form.find(exactText("CVC/CVV")).$(".input__control");
+    ElementsCollection formInputControl = $$(".input");
+    SelenideElement numberCardField = formInputControl.find(exactText("Номер карты")).$(".input__control");
+    SelenideElement monthField = formInputControl.find(exactText("Месяц")).$(".input__control");
+    SelenideElement yearField = formInputControl.find(exactText("Год")).$(".input__control");
+    SelenideElement ownerField = formInputControl.find(exactText("Владелец")).$(".input__control");
+    SelenideElement cvcCvvField = formInputControl.find(exactText("CVC/CVV")).$(".input__control");
+
+    ElementsCollection formInputTops = $$(".input__top");
+    SelenideElement cardNumberInputSub = formInputTops.find(exactText("Номер карты")).parent().$(".input__sub");
+    SelenideElement yearInputSub = formInputTops.find(exactText("Год")).parent().$(".input__sub");
+    SelenideElement cvcInputSub = formInputTops.find(exactText("CVC/CVV")).parent().$(".input__sub");
+    SelenideElement holderInputSub = formInputTops.find(exactText("Владелец")).parent().$(".input__sub");
+    SelenideElement monthInputSub = formInputTops.find(exactText("Месяц")).parent().$(".input__sub");
 
     private final SelenideElement continueButton = $$("button").find(exactText("Продолжить"));
 
     SelenideElement paymentOkWindow = $(".notification_status_ok");
     SelenideElement paymentErrorWindow = $(".notification_status_error");
+    //SelenideElement inputError = $(".input_invalid");
 
     public StartPage validBuyTour(Cards card) {
         numberCardField.setValue(card.number);
@@ -34,13 +43,6 @@ public class StartPage {
         return new StartPage();
     }
 
-    public void messageOfSuccessBuy() {
-        paymentOkWindow.waitUntil(Condition.visible, 15000);
-    }
-
-    public void failureMessage() {
-        paymentErrorWindow.waitUntil(Condition.visible, 15000);
-    }
 
     public BuyPage buyPage() {
         buyButton.click();
@@ -50,5 +52,35 @@ public class StartPage {
     public CreditPage creditPage() {
         creditButton.click();
         return new CreditPage();
+    }
+
+    public void messageOfSuccessBuy() {
+        paymentOkWindow.waitUntil(Condition.visible, 15000);
+    }
+
+    public void failureMessage() {
+        paymentErrorWindow.waitUntil(Condition.visible, 15000);
+    }
+
+    public void messageOfIncorrectInputCard(String message) {
+        cardNumberInputSub.shouldHave(exactText(message));
+    }
+
+    public void messageOfIncorrectInputMonth(String message) {
+        monthInputSub.shouldHave(exactText(message));
+    }
+
+    public void messageOfIncorrectInputYear(String message) {
+        yearInputSub.shouldHave(exactText(message));
+    }
+
+    public void messageOfIncorrectInputCVC(String message) {
+        cvcInputSub.shouldHave(exactText(message));
+    }
+
+
+
+    public void messageOfIncorrectInputHolder(String message) {
+        holderInputSub.shouldHave(exactText(message));
     }
 }
--- src/test/java/test/TestsPurchaseTour.java
+++ src/test/java/test/TestsPurchaseTour.java
@@ -43,7 +43,6 @@ public class TestsPurchaseTour {
         val approvedCard = Cards.getApprovedCards();
         startPage.validBuyTour(approvedCard);
         startPage.messageOfSuccessBuy();
-        System.out.println(SqlHelper.getStatusPurchase());
         assertEquals(SqlHelper.getStatusPurchase(), "APPROVED");
     }
 
@@ -52,11 +51,143 @@ public class TestsPurchaseTour {
     void tourPurchaseShouldUncorrected() throws SQLException {
         val startPage = new StartPage();
         startPage.buyPage();
-        val approvedCard = Cards.getDeclinedCards();
-        startPage.validBuyTour(approvedCard);
+        val declinedCards = Cards.getDeclinedCards();
+        startPage.validBuyTour(declinedCards);
         startPage.failureMessage();
+        startPage.messageOfIncorrectInputMonth("Неверный формат");
+
         assertEquals(SqlHelper.getStatusPurchase(), "DECLINED");
     }
+
+    @Test
+    @DisplayName("3. Отправка формы с незаполненным полем Номер карты")
+    void tourShouldNotPurchaseWithoutNumber () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCard("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("4. Отправка формы с незаполненным полем Месяц")
+    void tourShouldNotPurchaseWithoutMonth () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setMonth("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputMonth("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("5. Отправка формы с незаполненным полем Год")
+    void tourShouldNotPurchaseWithoutYear () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setYear("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputYear("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("6. Отправка формы с незаполненным полем Владелец")
+    void tourShouldNotPurchaseWithoutHolder () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setOwner("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputHolder("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("7. Отправка формы с незаполненным полем CVC/CVV")
+    void tourShouldNotPurchaseWithoutCVC () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setCvcCvv("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("8. Отправка пустой формы")
+    void tourShouldNotPurchaseWithoutAll () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("");
+        approvedCard.setMonth("");
+        approvedCard.setYear("");
+        approvedCard.setOwner("");
+        approvedCard.setCvcCvv("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCard ("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputMonth("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputYear("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputHolder("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputCVC("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("9. Отправка формы с с невалидным значением \"Номера карты\" (14 знаков)")
+    void tourShouldNotPurchaseWithIncorrectNumber () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("4444 4444 4444 41");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("10. Отправка формы с с невалидным значением \"Месяца\" (число больше 12)")
+    void tourShouldNotPurchaseWithIncorrectMonth () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setMonth("23");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("11. Отправка формы с  невалидным значением \"Год\" (число меньше 20)")
+    void tourShouldNotPurchaseWithIncorrectYear () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setYear("18");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Истёк срок действия карты");
+    }
+
+    @Test
+    @DisplayName("12. Отправка формы с невалидным значением в поле CVC/CVV (2 знака)")
+    void tourShouldNotPurchaseWithIncorrectCVC () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setCvcCvv("12");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("13. Отправка формы с полем Владелец, заполненным кириллицей")
+    void tourShouldNotPurchaseWithIncorrectHolder () throws SQLException {
+        val startPage = new StartPage();
+        startPage.buyPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setOwner("Петров Иван");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
     @Test
     @DisplayName("14. Покупка тура в кредит с валидными данными")
     void tourPurchaseByCreditShouldCorrect() throws SQLException {
@@ -78,4 +209,134 @@ public class TestsPurchaseTour {
         startPage.failureMessage();
         assertEquals(SqlHelper.getStatusPurchaseByCredit(), "DECLINED");
     }
+
+    @Test
+    @DisplayName("16. Покупка тура в кредит. Отправка формы с незаполненным полем Номер карты")
+    void tourShouldNotPurchaseByCreditWithoutNumber () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCard("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("17. Покупка тура в кредит. Отправка формы с незаполненным полем Месяц")
+    void tourShouldNotPurchaseByCreditWithoutMonth () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setMonth("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputMonth("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("18. Покупка тура в кредит. Отправка формы с незаполненным полем Год")
+    void tourShouldNotPurchaseByCreditWithoutYear () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setYear("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputYear("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("19. Покупка тура в кредит. Отправка формы с незаполненным полем Владелец")
+    void tourShouldNotPurchaseByCreditWithoutHolder () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setOwner("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputHolder("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("20. Покупка тура в кредит. Отправка формы с незаполненным полем CVC/CVV")
+    void tourShouldNotPurchaseByCreditWithoutCVC () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setCvcCvv("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("21. Покупка тура в кредит. Отправка пустой формы")
+    void tourShouldNotPurchaseByCreditWithoutAll () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("");
+        approvedCard.setMonth("");
+        approvedCard.setYear("");
+        approvedCard.setOwner("");
+        approvedCard.setCvcCvv("");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCard ("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputMonth("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputYear("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputHolder("Поле обязательно для заполнения");
+        startPage.messageOfIncorrectInputCVC("Поле обязательно для заполнения");
+    }
+
+    @Test
+    @DisplayName("22. Покупка тура в кредит. Отправка формы с с невалидным значением \"Номера карты\" (14 знаков)")
+    void tourShouldNotPurchaseByCreditWithIncorrectNumber () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setNumber("4444 4444 4444 41");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("23. Покупка тура в кредит. Отправка формы с с невалидным значением \"Месяца\" (число больше 12)")
+    void tourShouldNotPurchaseByCreditWithIncorrectMonth () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setMonth("56");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("24. Покупка тура в кредит. Отправка формы с  невалидным значением \"Год\" (число меньше 20)")
+    void tourShouldNotPurchaseByCreditWithIncorrectYear () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setYear("19");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Истёк срок действия карты");
+    }
+
+    @Test
+    @DisplayName("25. Покупка тура в кредит. Отправка формы с невалидным значением в поле CVC/CVV (2 знака)")
+    void tourShouldNotPurchaseByCreditWithIncorrectCVC () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setCvcCvv("99");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
+    @Test
+    @DisplayName("26. Покупка тура в кредит. Отправка формы с полем Владелец, заполненным кириллицей")
+    void tourShouldNotPurchaseByCreditWithIncorrectHolder () throws SQLException {
+        val startPage = new StartPage();
+        startPage.creditPage();
+        val approvedCard = Cards.getApprovedCards();
+        approvedCard.setOwner("Ио Ким");
+        startPage.validBuyTour(approvedCard);
+        startPage.messageOfIncorrectInputCVC("Неверный формат");
+    }
+
 }
